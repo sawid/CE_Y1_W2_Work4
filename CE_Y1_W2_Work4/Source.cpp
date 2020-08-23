06ffcf18d55740287b6341cc28f7643b2c96132a
sawid <zasawidza@gmail.com>
2020-08-23 21:24:05 +0700

    Day One - Update

--- CE_Y1_W2_Work4/Source.cpp
+++ CE_Y1_W2_Work4/Source.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	int x;
-	scanf("%d",&x);
-
+	int n;
+	scanf_s("%d", &n);
+	if (n % 2 == 0) {
+		printf("Even Number");
+	}
+	else if (n % 2 != 0)
+	{
+		printf("Odd Number");
+	}
+	
 	return 0;
 }
